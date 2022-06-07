796c6f2c8f8aeb4c1450e1ade1e7cdc20b3de5a3
IpainI <90567777+IpainI@users.noreply.github.com>
2022-06-07 16:54:10 +0300

    Update gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,17 +1,10 @@
 import pprint
 
-
 from gendiff import generate_diff
-from gendiff.cli import pars_args
 
 
 def main():
-    file1, file2, out_format = pars_args()
-    tree = generate_diff(file1, file2, out_format)
-    if out_format == 'json':
-        pprint.pprint(tree)
-    else:
-        print(tree)
+    generate_diff(file1, file2, out_format)
 
 
 if __name__ == '__main__':
