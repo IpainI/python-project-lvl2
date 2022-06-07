e5957d5e525e747132c67d49bc2dfdff0a8ad221
IpainI <90567777+IpainI@users.noreply.github.com>
2022-06-07 16:48:19 +0300

    Update parser.py

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -6,6 +6,6 @@ import yaml
 def parse(content, extension):
     if extension == 'json':
         return json.loads(content)
-    elif content in ('yaml', 'yml'):
+    elif extension in ('yaml', 'yml'):
         return yaml.safe_load(content)
     raise ValueError(f'Files with extension {extension} are not supported')
