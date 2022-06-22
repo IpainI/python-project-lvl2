8b0b818424ec5620a9d344dc4c7081771b0a288f
IpainI <90567777+IpainI@users.noreply.github.com>
2022-06-22 18:49:43 +0300

    Update generate_diff.py

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -2,7 +2,7 @@ from gendiff import formatters
 from gendiff.loader import get_file_content
 
 
-def generate_diff(file1, file2, output_format='plain'):
+def generate_diff(file1, file2, output_format='pretty'):
     content1 = get_file_content(file1)
     content2 = get_file_content(file2)
     formatter = formatters.choose(output_format)
