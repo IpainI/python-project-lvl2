6b65f1f95d62fa2aab08e4e291f118c3c4664ca5
IpainI <90567777+IpainI@users.noreply.github.com>
2022-06-30 18:10:40 +0300

    Update formatters.py

--- gendiff/formatters/formatters.py
+++ gendiff/formatters/formatters.py
@@ -4,8 +4,9 @@ from gendiff.formatters import plain, pretty, json
 def choose(out_format):
     formatters = {
         'plain': plain,
-        'stylish': pretty,
+        'pretty': pretty,
         'json': json,
+        'stylish' : pretty,
     }
     if not out_format:
         return stylish
